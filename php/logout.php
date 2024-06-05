606d0a4a833d18c16e45bef89342a286d7ea929b
bistaanj <bistaanj@gmail.com>
2024-06-05 10:00:59 -0400

    Multiple Login from same device managed.

--- php/logout.php
+++ php/logout.php
@@ -1,11 +1,16 @@
 <?php
+session_start();
 require_once '../php/connection.php';
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     // Query to delete session in the database
-    $querry = "DELETE from tbl_session where session_id = ? ";
+
+
+    $querry = "DELETE from tbl_session where session_id = ?";
     $bind_statement = $connect->prepare("$querry");
-    $bind_statement->bind_param("s", $_GET["session_id"]);
+    $bind_statement->bind_param("s", $_SESSION["session_id"]);
     $result = $bind_statement->execute();
+    $connect->close();
+
     if ($result) {
         session_start();
         session_unset();
@@ -13,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         header("Location: ../pages/login.php");
         exit();
     } else {
-        header("Location: ../pages/login.php");
+        echo "Invalid Logout";
     }
 }
 ?>
