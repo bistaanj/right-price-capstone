93c517db74af8f73ca4dd318044dd4db72bd4441
bistaanj <bistaanj@gmail.com>
2024-07-17 17:26:00 -0400

    Created includes for the header and footer.

--- pages/blogs.php
+++ pages/blogs.php
@@ -12,45 +12,7 @@
 </head>
 
 <body>
-<header class="d-flex justify-content-between align-items-center p-3 bg-light">
-        <div class="logo">
-            <img src="../images/RightPriceLogo.jpeg" alt="Logo">
-        </div>
-        <nav>
-            <ul class="nav">
-                <li class="nav-item text-center">
-                    <a class="nav-link d-flex flex-column align-items-center" href="dashboard.php">
-                        <i class="bi bi-speedometer2"></i>
-                        <span>Dashboard</span>
-                    </a>
-                </li>
-                <li class="nav-item text-center">
-                    <a class="nav-link d-flex flex-column align-items-center" href="wishlist.php">
-                        <i class="bi bi-bag-check"></i>
-                        <span>Wishlist</span>
-                    </a>
-                </li>
-                <li class="nav-item text-center">
-                    <a class="nav-link d-flex flex-column align-items-center" href="market.php">
-                        <i class="bi bi-shop"></i>
-                        <span>Market</span>
-                    </a>
-                </li>
-                <li class="nav-item text-center">
-                    <a class="nav-link d-flex flex-column align-items-center" href="blogs.php">
-                        <i class="bi bi-pencil-square"></i>
-                        <span>Blogs</span>
-                    </a>
-                </li>
-                <li class="nav-item text-center">
-                    <a class="nav-link d-flex flex-column align-items-center" href="../php/logout.php">
-                        <i class="bi bi-box-arrow-right"></i>
-                        <span>Logout</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    </header>
+    <?php include '../includes/navigation.php'; ?>  
     <main class="container mt-4 flex-grow-1">
         <div class="search-bar d-flex justify-content-center mb-4">
             <input type="text" class="form-control rounded-pill" placeholder="Search" style="max-width: 600px;">
--- pages/dashboard.php
+++ pages/dashboard.php
@@ -10,45 +10,7 @@
     <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
-    <header class="d-flex justify-content-between align-items-center p-3 bg-light">
-        <div class="logo">
-            <img src="../images/RightPriceLogo.jpeg" alt="Logo">
-        </div>
-        <nav>
-            <ul class="nav">
-                <li class="nav-item text-center">
-                    <a class="nav-link d-flex flex-column align-items-center" href="dashboard.php">
-                        <i class="bi bi-speedometer2"></i>
-                        <span>Dashboard</span>
-                    </a>
-                </li>
-                <li class="nav-item text-center">
-                    <a class="nav-link d-flex flex-column align-items-center" href="wishlist.php">
-                        <i class="bi bi-bag-check"></i>
-                        <span>Wishlist</span>
-                    </a>
-                </li>
-                <li class="nav-item text-center">
-                    <a class="nav-link d-flex flex-column align-items-center" href="../php/marketProducts.php">
-                        <i class="bi bi-shop"></i>
-                        <span>Market</span>
-                    </a>
-                </li>
-                <li class="nav-item text-center">
-                    <a class="nav-link d-flex flex-column align-items-center" href="blogs.php">
-                        <i class="bi bi-pencil-square"></i>
-                        <span>Blogs</span>
-                    </a>
-                </li>
-                <li class="nav-item text-center">
-                    <a class="nav-link d-flex flex-column align-items-center" href="../php/logout.php">
-                        <i class="bi bi-box-arrow-right"></i>
-                        <span>Logout</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    </header>
+    <?php include '../includes/navigation.php'; ?>  
     <main class="container-fluid mt-4">
         <div class="row">
             <div class="col-12 text-center overview-heading">
@@ -60,7 +22,11 @@
                 <div class="sidebar p-3 rounded">
                     <div class="username mb-4 d-flex align-items-center">
                         <i class="bi bi-person-circle" style="margin-right: 10px;"></i>
-                        <span style="color: yellow;">Username</span>
+                        
+                        <span style="color: yellow;">
+                            <?php
+                            session_start();
+                            echo $_SESSION['username']; ?> </span>
                     </div>
                     <ul class="nav flex-column">
                         <li class="nav-item mb-3 d-flex align-items-center">
@@ -108,22 +74,8 @@
             </section>
         </div>
     </main>
-    <footer class="footer mt-5">
-        <div class="container d-flex justify-content-between align-items-center">
-            <div class="footer-left">
-                <img src="Images/RightPriceLogo.jpeg" alt="Logo" class="footer-logo">
-                <h4>Right Price</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
-            <div class="footer-right">
-                <ul class="footer-links list-inline">
-                    <li class="list-inline-item"><a href="#">About</a></li>
-                    <li class="list-inline-item"><a href="#">Contact</a></li>
-                    <li class="list-inline-item"><a href="#">Careers</a></li>
-                </ul>
-            </div>
-        </div>
-    </footer>
+    <?php include '../includes/footer.php'; ?>  
+    
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
