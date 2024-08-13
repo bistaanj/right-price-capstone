8a9fe97ba5c21b047e413e603895e674bcbff709
anuragturke24 <76623777+anuragturke24@users.noreply.github.com>
2024-08-12 23:46:44 -0400

    Changes to improve the ui of the website

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,23 +1,23 @@
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
 <script>
-        function confirmDelete(form) {
-            swal({
-                title: "Logout?",
-                text: "Any incomplete form will not be saved.",
-                icon: "warning",
-                buttons: true,
-                dangerMode: true,
-            })
-            .then((willDelete) => {
-                if (willDelete) {
-                    form.submit();
-                } else {
-                    // swal("Session is still live.");
-                }
-            });
-        }
-    </script>
+    function confirmDelete(form) {
+        swal({
+            title: "Logout?",
+            text: "Any incomplete form will not be saved.",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        })
+        .then((willDelete) => {
+            if (willDelete) {
+                form.submit();
+            } else {
+                // swal("Session is still live.");
+            }
+        });
+    }
+</script>
 
 <header class="d-flex justify-content-between align-items-center p-3 bg-light">
     <div class="logo">
@@ -45,23 +45,21 @@
             </li>
             <li class="nav-item text-center">
                 <a class="nav-link d-flex flex-column align-items-center" href="blogs.php">
-                    <i class="bi bi-pencil-square"></i>
+                    <i class="bi bi-book"></i>
                     <span>Blogs</span>
                 </a>
             </li>
             <li class="nav-item text-center">
-
-            <form action='../php/logout.php' method='post' class='mb-2'>
-                <button type='button' class='btn btn-primary d-flex align-items-center justify-content-center' onclick='confirmDelete(this.form)'>
-                    <i class="bi bi-box-arrow-right"></i>
-                </button>
-            </form>
-                
+                <form action='../php/logout.php' method='post' class='mb-2'>
+                    <a class="nav-link d-flex flex-column align-items-center" href="#" onclick='confirmDelete(this.form)'>
+                        <i class="bi bi-box-arrow-right"></i>
+                        <span>Logout</span>
+                    </a>
+                </form>
             </li>
         </ul>
     </nav>
 </header>
-
 
 <!-- Include necessary JS files -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
